42234d5773d4a3b301dcb68a6b98f3e40ab2e421
orxan4 <150265257+orxan4@users.noreply.github.com>
2025-01-27 07:00:49 +0400

    edit dashboard.ts

--- src/app/modules/dashboard-page/dashboard-page.component.ts
+++ src/app/modules/dashboard-page/dashboard-page.component.ts
@@ -1,55 +1,17 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
-import { MatDrawer, MatDrawerContainer } from "@angular/material/sidenav";
-import { MatIcon } from "@angular/material/icon";
-import { MatDialog } from "@angular/material/dialog";
-import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from "@angular/cdk/menu";
-import { firstValueFrom } from "rxjs";
+import { Component } from '@angular/core';
+import { NgForOf, TitleCasePipe } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-import { GlobalService } from "../../common/services/global.service";
-import { LessonDialogComponent } from "./components/lessons/lesson-dialog/lesson-dialog.component";
-import { ExamDialogComponent } from "./components/exams/components/exam-dialog/exam-dialog.component";
-import { StudentDialogComponent } from "./components/students/student-dialog/student-dialog.component";
+import { GlobalService } from '../../common/services/global.service';
 
 @Component({
-  selector: "app-dashboard-page",
+  selector: 'app-dashboard-page',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    RouterOutlet,
-    CdkMenuTrigger,
-    CdkMenu,
-    CdkMenuItem,
-    MatIcon,
-    MatDrawerContainer,
-    MatDrawer,
-  ],
-  templateUrl: "./dashboard-page.component.html",
+  imports: [NgForOf, RouterLink, RouterLinkActive, RouterOutlet, TitleCasePipe],
+  templateUrl: './dashboard-page.component.html',
 })
-export class DashboardPageComponent implements OnInit {
-  readonly dialog = inject(MatDialog);
+export class DashboardPageComponent {
+  routes = ['exams', 'lessons', 'students'];
+
   constructor(public globalService: GlobalService) {}
-
-  ngOnInit(): void {}
-
-  async openCreateDialog(
-    tableName: "lessons" | "exams" | "students",
-  ): Promise<void> {
-    const data = { mode: "create", tableName: tableName, obj: null };
-    let component = null;
-    if (tableName === "lessons") component = LessonDialogComponent;
-    if (tableName === "exams") component = ExamDialogComponent;
-    if (tableName === "students") component = StudentDialogComponent;
-
-    const dialogRef = this.dialog.open(component, {
-      data,
-      disableClose: true,
-      minWidth: 280,
-      maxWidth: 1200,
-      width: "100%",
-    });
-
-    await firstValueFrom(dialogRef.afterClosed());
-  }
 }
